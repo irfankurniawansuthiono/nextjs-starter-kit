f9b725b03f79dfe80f49d51457fab7df89adfee6
chndrwali <chndrwali@gmail.com>
2026-03-08 17:49:50 +0700

    feat: Add front-end development flow documentation and configure Next.js to allow images from utfs.io.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        //change to specific domain in uploadthing
+      },
+    ],
+  },
 };
 
 export default nextConfig;
